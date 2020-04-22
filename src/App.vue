cec9958fb655572b0d1c85201862088ae7ebbd37
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2020-04-22 21:45:37 +0400

    next

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
   <v-app>
-    <v-navigation-drawer app ></v-navigation-drawer>
-    <v-toolbar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-navigation-drawer
+            app
+            temporary
+            v-model="drawer"
+    >
+      <v-list>
+        <v-list-item
+                v-for="link of links"
+                :key="link.title"
+                :to="link.url"
+        >
+          <v-list-item-icon>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon
+              class="hidden-md-and-up"
+              @click="drawer = !drawer"/>
       <v-toolbar-title>Ad application</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
+        <v-btn
+                v-for="link in links"
+                :key="link.title"
+                :to="link.url"
+                text>
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}</v-btn>
       </v-toolbar-items>
-    </v-toolbar>
+    </v-app-bar>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -24,9 +52,15 @@ export default {
   components: {
     //HelloWorld,
   },
-
   data: () => ({
-    //
+      drawer: null,
+      links: [
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration', icon: 'mdi-face', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-bookmark', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-plus', url: '/new'},
+        {title: 'My ads', icon: 'mdi-dialpad', url: '/list'}
+      ]
   }),
 };
 </script>
