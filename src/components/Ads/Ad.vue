14cb81cdaa9308e6a1090345fd894460e54bf19c
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2020-04-26 18:41:41 +0400

    Other components

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,17 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary">lorem</h1>
+                        <p>Lorem ipsum</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="warning">Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,35 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>AdList</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">My ads</h1>
+                <v-card
+                        v-for="ad in ads"
+                        :key="ad.id"
+                        class="elevation-10 mb-3">
+                    <v-layout row>
+                        <v-flex xs4>
+                            <v-avatar
+                                    class="ml-sm-2"
+                                    size="150"
+                                    tile
+                            >
+                                <v-img :src="ad.imageSrc"></v-img>
+                            </v-avatar>
+                        </v-flex>
+                        <v-flex xs8>
+                            <v-card-text>
+                                <h2 class="text--primary">{{ad.title}}</h2>
+                                <p>{{ad.description}}</p>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-btn
+                                        :to="'/ad/' + ad.id"
+                                        class="info">Open</v-btn>
+                            </v-card-actions>
+                        </v-flex>
+                    </v-layout>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
@@ -12,7 +39,15 @@
     export default {
         name: "AdList",
         data: () => ({
-
+                ads: [
+                    {
+                        title: 'First ad',
+                        description: 'Hello i am description',
+                        promo: false,
+                        imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+                        id: '123'
+                    }
+                ]
         })
     }
 </script>
